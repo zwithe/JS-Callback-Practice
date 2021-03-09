b3a10b77a8005980bbdb5b47a87fff3853a14959
Joshua Miles <joshua.m@hackerusa.com>
2021-03-09 13:02:58 -0600

    -

---
+++ move.js
@@ -0,0 +1,12 @@
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    return {
+        to: moveToCoordinates
+    }
+}
